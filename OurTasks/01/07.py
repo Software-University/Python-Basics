36638140f40a193125ae47668bedae722352e46c
Alex Tsvetanov <alex.tsvetanov.2002@gmail.com>
2017-02-05 17:11:33 +0200

    first commit

--- OurTasks/01/07.py
+++ OurTasks/01/07.py
@@ -1,5 +1,5 @@
 for i in range (2, 1002):
-    if divmod (i, 2) == 0:
+    if i % 2 == 0:
         print (i)
     else:
         print (-i)
---
+++ OurTasks/02/09.py
@@ -0,0 +1,5 @@
+a = float (input ())
+b = float (input ())
+h = float (input ())
+
+print ("{0:.2f}".format ((a + b) * h / 2))
