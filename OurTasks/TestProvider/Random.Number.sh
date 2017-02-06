99434c70e2d36ce490c1d1e5ee3e2f843e53251d
Alex Tsvetanov <alex.tsvetanov.2002@gmail.com>
2017-02-06 11:12:19 +0200

    first commit

--- OurTasks/TestProvider/Random.Number.sh
+++ OurTasks/TestProvider/Random.Number.sh
@@ -3,5 +3,5 @@ if [[ $(($RANDOM % 2)) == 1 ]]
 then
 	echo -n "-"
 fi
-echo "`cat /dev/urandom | tr -dc '0-9' | fold -w $(($RANDOM % $1)) | head -n 1`"
+echo "`cat /dev/urandom | tr -dc '0-9' | fold -w $(($RANDOM % $1 + 1)) | head -n 1`"
 
