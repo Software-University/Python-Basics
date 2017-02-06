bc68f0399eebc5810e8fbba5e019b6f1c1f9fd21
Alex Tsvetanov <alex.tsvetanov.2002@gmail.com>
2017-02-06 11:10:09 +0200

    first commit

--- OurTasks/TestProvider/Random.Decimal.sh
+++ OurTasks/TestProvider/Random.Decimal.sh
@@ -6,7 +6,7 @@ then
 		echo -n "-"
 	fi
 fi
-echo -n "`cat /dev/urandom | tr -dc '0-9' | fold -w $(($RANDOM % $1)) | head -n 1`"
+echo -n "`cat /dev/urandom | tr -dc '0-9' | fold -w $(($RANDOM % $1 + 1)) | head -n 1`"
 echo -n "."
-echo "`cat /dev/urandom | tr -dc '0-9' | fold -w $(($RANDOM % $2)) | head -n 1`"
+echo "`cat /dev/urandom | tr -dc '0-9' | fold -w $(($RANDOM % $2 + 1)) | head -n 1`"
 
